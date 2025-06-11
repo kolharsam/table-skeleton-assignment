9d3d795752067296a6d9d016fb5628519bea76a3
Sameer Kolhar <kolhar730@gmail.com>
2025-06-11 00:05:58 -0700

    update font size of label

--- src/app/modules/formBuilder/components/label.jsx
+++ src/app/modules/formBuilder/components/label.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Label = ({ children, ...props }) => {
   return (
-    <label className="block font-medium text-sm text-zinc-800" {...props}>
+    <label className="block font-medium text-md text-zinc-800" {...props}>
       {children}
     </label>
   );
