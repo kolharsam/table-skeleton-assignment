1f393bf1c532b2bf95f102fcc71264260c4edb32
Sameer Kolhar <kolhar730@gmail.com>
2025-06-10 20:14:21 -0700

    update index.jsx in components
    label component was newly added here

--- src/app/modules/formBuilder/components/index.jsx
+++ src/app/modules/formBuilder/components/index.jsx
@@ -6,5 +6,6 @@ import Button from "./button";
 import Checkbox from "./checkbox";
 import Select from "./select";
 import MultiSelect from "./multi-select";
+import Label from "./label";
 
-export { TextBox, Box, Button, Checkbox, Select, MultiSelect };
+export { TextBox, Box, Button, Checkbox, Select, MultiSelect, Label };
