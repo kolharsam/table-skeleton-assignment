186eb7ed1f4d5089558b58403cebf5e861f17e98
Sameer Kolhar <kolhar730@gmail.com>
2025-06-11 00:07:17 -0700

    fix arrow issue in select component

--- src/app/modules/formBuilder/components/select.jsx
+++ src/app/modules/formBuilder/components/select.jsx
@@ -1,21 +1,24 @@
 "use client";
 import React from "react";
 import { cn } from "./utils";
+import ChevronDown from "../icons/ChevronDown";
 
 export default function Select({ className, options, ...props }) {
   return (
-    <select
-      className={cn(
-        "w-full h-9 p-2 border rounded-md bg-white focus:ring-2 focus:border-zinc-800 disabled:opacity-50",
-        className
-      )}
-      {...props}
-    >
-      {options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <div className={cn("relative", className)}>
+      <select
+        className="w-full h-auto appearance-none p-2 pr-8 border rounded-md bg-white focus:ring-2 focus:border-zinc-800 disabled:opacity-50 outline-none"
+        {...props}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <div className="absolute right-2 top-1/2 -translate-y-1/2 pointer-events-none text-zinc-800">
+        <ChevronDown />
+      </div>
+    </div>
   );
 }
