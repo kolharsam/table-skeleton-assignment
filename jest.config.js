4c982fb8746bb2e81fd54c0c11597d1288e5d4e3
Sameer Kolhar <kolhar730@gmail.com>
2025-06-11 00:31:14 -0700

    add test setup

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/index.{js,jsx,ts,tsx}",
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
