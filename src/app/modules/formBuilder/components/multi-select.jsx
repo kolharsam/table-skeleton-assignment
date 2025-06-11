f7aa4e00fce6193b6422b4117cf79528b12c4466
Sameer Kolhar <kolhar730@gmail.com>
2025-06-11 01:32:26 -0700

    make style and event handlers to multi-select
    - add `mousedown` and `esc` key event handlers to ensure good user experience and interactions
    - update styles to ensure that whole div container of the options is selectable
    - function definitions were made cleaner and more performant with the use of `useMemo` and `useCallback`

--- src/app/modules/formBuilder/components/multi-select.jsx
+++ src/app/modules/formBuilder/components/multi-select.jsx
@@ -1,5 +1,12 @@
 "use client";
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useMemo,
+} from "react";
 import { cn } from "./utils";
 import { ChevronDown } from "../icons";
 
@@ -12,12 +19,17 @@ export default function MultiSelect({
 }) {
   const [isOpen, setIsOpen] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
-  // NOTE: This is to prevent the component from rendering before
+  // NOTE: isMounted is to prevent the component from rendering before
   // the DOM is ready and cause hydration error
+  const containerRef = useRef(null);
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   const handleToggle = useCallback(
     (option) => {
@@ -29,21 +41,46 @@ export default function MultiSelect({
     [value, onChange]
   );
 
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    const handleEscapeKey = (event) => {
+      if (event.key === "Escape") {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+      document.addEventListener("keydown", handleEscapeKey);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [isOpen]);
+
   const displayText = useMemo(() => {
     switch (value.length) {
       case 0:
         return "Select options...";
       case 1:
-        return "1 choice selected";
+        return "1 option selected";
       default:
-        return `${value.length} choices selected`;
+        return `${value.length} options selected`;
     }
   }, [value]);
 
-  if (!isMounted) return null;
-
   return (
-    <div className={cn("relative", className)}>
+    <div ref={containerRef} className={cn("relative", className)}>
       <button
         type="button"
         onClick={() => setIsOpen(!isOpen)}
@@ -51,9 +88,7 @@ export default function MultiSelect({
         {...props}
       >
         <span
-          className={`truncate max-w-[60%] ${
-            value.length === 0 ? "text-gray-500" : "text-black"
-          }`}
+          className={`${value.length === 0 ? "text-gray-500" : "text-black"}`}
         >
           {displayText}
         </span>
@@ -71,18 +106,18 @@ export default function MultiSelect({
           {options.map((option) => (
             <div
               key={`${option}-multi-select-checkbox`}
-              className="flex items-center gap-2 p-2 hover:bg-gray-100"
+              className="flex items-center gap-2 hover:bg-gray-100"
             >
               <input
                 type="checkbox"
                 checked={value.includes(option)}
                 onChange={() => handleToggle(option)}
-                className="h-4 w-4 rounded border hover:cursor-pointer"
+                className="h-4 w-4 rounded border hover:cursor-pointer ml-2 mt-2 mb-2"
                 id={`${option}-multi-select-checkbox`}
               />
               <label
                 htmlFor={`${option}-multi-select-checkbox`}
-                className="text-sm w-full hover:cursor-pointer select-none"
+                className="text-sm w-full hover:cursor-pointer select-none pt-2 pb-2 pr-2"
               >
                 {option}
               </label>
